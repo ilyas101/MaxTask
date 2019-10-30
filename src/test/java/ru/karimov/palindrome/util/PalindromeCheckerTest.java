f7e338eebe28a9d048af530b6a1a812e77e93db7
ilyas <ikarimov@kazan.amfitel.ru>
2019-10-30 11:14:43 +0300

    add functionality

---
+++ src/test/java/ru/karimov/palindrome/util/PalindromeCheckerTest.java
@@ -0,0 +1,24 @@
+package ru.karimov.palindrome.util;
+
+import org.junit.Test;
+import ru.karimov.palindrome.util.palindromChecker.PalindromeChecker;
+
+import static org.junit.Assert.*;
+/**
+ * Created by ikarimov on 30.10.2019.
+ */
+public class PalindromeCheckerTest {
+    PalindromeChecker palindromeChecker = new PalindromeChecker();
+
+    @Test
+    public void testIsValidPalindrome() throws Exception {
+        String testValid = "asd f dsa";
+        assertTrue("The phrase must be a palindrome", palindromeChecker.isPalindrome(testValid));
+    }
+
+    @Test
+    public void testIsNotValidPalindrome() throws Exception {
+        String testNotValid = "asd f dsa1";
+        assertFalse("The phrase should not be a palindrome", palindromeChecker.isPalindrome(testNotValid));
+    }
+}
