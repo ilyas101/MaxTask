36688e1db7905b86cd9eb6f5fa5400cb656886a1
ilyas <your_email@whatever.com>
2019-12-17 22:18:23 +0300

    refactoring

--- src/test/java/ru/karimov/palindrome/repository/simpleRepository/SimpleUserRepositoryImplTest.java
+++ src/test/java/ru/karimov/palindrome/repository/simpleRepository/SimpleUserRepositoryImplTest.java
@@ -1,21 +1,19 @@
 package ru.karimov.palindrome.repository.simpleRepository;
 
+import org.junit.Before;
+import org.junit.Test;
 import ru.karimov.palindrome.model.User;
-
 import java.util.TreeSet;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.*;
 /**
  * Created by 777 on 30.11.2019.
  */
 public class SimpleUserRepositoryImplTest {
-    SimpleUserRepositoryImpl simpleUserRepository = new
-            SimpleUserRepositoryImpl();
+    SimpleUserRepositoryImpl simpleUserRepository = new SimpleUserRepositoryImpl();
 
-    @Test
-    public void findLeaders_userBobs_first() {
+    @Before
+    public void init() {
         User user1 = new User("Bob");
         user1.setPoins(2);
         User user2 = new User("Bober");
@@ -32,8 +30,16 @@ public class SimpleUserRepositoryImplTest {
         simpleUserRepository.save(user3);
         simpleUserRepository.save(user4);
         simpleUserRepository.save(user5);
+    }
 
+    @Test
+    public void findLeaders_userBobsShouldFirst_true() {
         TreeSet<User> leaderSet = simpleUserRepository.findLeaders();
         assertTrue("Bobs".equals(leaderSet.first().getName()));
+    }
+
+    @Test
+    public void findUserByName_validUser_notNull() {
+        assertNotNull(simpleUserRepository.findUserByName("Bobik"));
     }
 }
--- src/test/java/ru/karimov/palindrome/util/PalindromeCheckerTest.java
+++ src/test/java/ru/karimov/palindrome/util/PalindromeCheckerTest.java
@@ -1,9 +1,9 @@
 package ru.karimov.palindrome.util;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import ru.karimov.palindrome.util.palindromChecker.PalindromeChecker;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 /**
  * Created by ikarimov on 30.10.2019.
  */
@@ -13,12 +13,12 @@ public class PalindromeCheckerTest {
     @Test
     public void isPalindrome_validPalindrome_true() throws Exception {
         String testValid = "asd f dsa";
-        assertTrue(palindromeChecker.isPalindrome(testValid), "The phrase must be a palindrome");
+        assertTrue("The phrase must be a palindrome", palindromeChecker.isPalindrome(testValid));
     }
 
     @Test
     public void isPalindrome_notValidPalindrome_false() throws Exception {
         String testNotValid = "asd f dsa1";
-        assertFalse(palindromeChecker.isPalindrome(testNotValid), "The phrase should not be a palindrome");
+        assertFalse("The phrase should not be a palindrome", palindromeChecker.isPalindrome(testNotValid));
     }
 }
